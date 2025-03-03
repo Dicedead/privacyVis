847120df69a1e9c8231bd83f611fe02ab727d1dc
dicedead <dicedead@gmail.com>
2025-03-03 18:39:25 +0100

    adding multiple region drawing feature

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import numpy as np
+
 from differential_privacy import *
 from regions import *
 
 eps = 2
 delta = 0.01
 constraints = region_from_dp_params(eps, delta)
-draw_region_from_constraints(constraints)
+# draw_single_region_from_constraints(constraints)
+# draw_single_region_from_constraints(region_from_f_dp(lambda fp: np.maximum(np.maximum(0, 1 - delta - np.exp(eps) * fp), np.exp(-eps) * (1 - delta - fp))))
+# draw_single_region_from_constraints(region_from_gaussian_dp(1))
 
-# TODO make sure region_from_f_dp outputs the same as region_from_diff_priv with correct function given
+fig = MultiRegionFigure()
+fig.add_region(region_from_gaussian_dp(1), "1")
+fig.add_region(region_from_gaussian_dp(0.4), "0.4")
+fig.add_region(region_from_gaussian_dp(0.1), "0.1")
+fig.finish_figure()
+fig.show_figure()
--- src/model/regions.py
+++ src/model/regions.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from definitions import Constraint
 from typing import Sequence
 from functools import reduce
 
-def draw_region_from_constraints(
+def draw_single_region_from_constraints(
         constraints: Sequence[Constraint],
         start_grid=0,
         stop_grid=1,
         grid_res=500,
-        cmap="Greys",
-        alpha=0.3
+        cmap="Blues",
+        alpha=1
 ):
-    # TODO make this function not show directly + possibility to plot multiple regions on same figure + make figure larger
     """
     Draw a region from a list of constraints.
 
@@ -49,3 +49,44 @@ def draw_region_from_constraints(
     plt.xlabel('FN')
     plt.ylabel('FP')
     plt.show()
+
+
+class MultiRegionFigure:
+    def __init__(self,
+        start_grid=0,
+        stop_grid=1,
+        grid_res=600,
+        cmap="Blues",
+        alpha=0.2
+    ):
+        d = np.linspace(start_grid, stop_grid, num=grid_res)
+        self._x, self._y = np.meshgrid(d, d)
+        self._start = start_grid
+        self._stop = stop_grid
+        self._cmap = cmap
+        self._alpha = alpha
+        self._labels = []
+
+    def add_region(self, constraints: Sequence[Constraint], label: str):
+        applied_constraints = [constraint(self._x, self._y) for constraint in constraints]
+        plt.imshow((reduce(lambda c1, c2: c1 & c2, applied_constraints)).astype(float),
+                   extent=(self._start, self._stop, self._start, self._stop),
+                   origin="lower", cmap=self._cmap, alpha=self._alpha)
+        self._labels.append(label)
+
+    def finish_figure(self):
+        # TODO add option for title
+        patches = [mpatches.Patch(color=[0.1,0.1,1.0, self._alpha ** i], label=lab)
+                   for i, lab in enumerate(self._labels)]
+        plt.legend(handles=patches)
+        plt.xlim(self._start, self._stop)
+        plt.ylim(self._start, self._stop)
+        plt.xlabel('FN')
+        plt.ylabel('FP')
+
+    def show_figure(self):
+        plt.show()
+
+    def save_figure(self, path):
+        plt.savefig(path=path)
+
