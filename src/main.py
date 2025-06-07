b152f5dcfc137774a4afc2b967669ad86f40b3e3
dicedead <dicedead@gmail.com>
2025-06-07 20:26:19 +0200

    added main menu

--- src/main.py
+++ src/main.py
@@ -1,158 +1,67 @@
-from gaussian_mechanism import GaussianMechanism
-from laplace_mechanism import *
-from randomized_response import RandomizedResponse
-from regions import *
+import tkinter as tk
+import tkinter.ttk as ttk
 
-def f_eps_delta_dp(eps, delta):
-    return lambda fp: np.maximum(np.maximum(0, 1 - delta - np.exp(eps) * fp), np.exp(-eps) * (1 - delta - fp))
+from histogram import DPHistogram
+from mean import DPMean
+from privacy_window import PrivacyWindow
+from utility_window import UtilityWindow
 
-def draw_multiple_regions():
-    eps = 0.3
-    delta = 0.3
-    constraints = region_from_dp_params(eps, delta)
-    draw_single_region_from_constraints(constraints)
-    draw_single_region_from_constraints(region_from_f_dp(f_eps_delta_dp(eps, delta)))
-    draw_single_region_from_constraints(region_from_gaussian_dp(1))
-    draw_single_region_from_constraints(region_from_dp_composition_exact(eps, delta, 3))
+class MainWindow:
+    def __init__(self):
+        self._window = tk.Tk()
+        self._window.configure(background="white")
+        self._window.title("Differential privacy")
+        self._window.geometry("250x150")
 
-def gdp():
-    fig = MultiRegionFigure()
-    fig.add_region(region_from_gaussian_dp(0.4), "0.4")
-    fig.add_region(region_from_gaussian_dp(0.1), "0.1")
-    fig.add_region(region_from_gaussian_dp(2), "2")
-    fig.add_region(region_from_gaussian_dp(1), "1")
-    fig.finish_figure()
-    fig.show_figure()
+        self._window.rowconfigure(0, weight=1)
+        self._window.rowconfigure(1, weight=1)
+        self._window.rowconfigure(2, weight=1)
+        self._window.rowconfigure(3, weight=1)
+        self._window.columnconfigure(0, weight=1)
 
-def composition_comparison():
-    eps = 0.3
-    delta = 0.3
-    k = 3
-    fig = MultiRegionFigure()
-    fig.add_region(region_from_dp_composition_basic(eps, delta, k), "basic")
-    fig.add_region(region_from_dp_composition_exact(eps, delta, k), "exact")
-    fig.finish_figure(title="Comparison of basic and exact composition theorems")
-    fig.show_figure()
+        self._build_button_privacy()
+        self._build_combob_utility()
 
-def laplacemech():
-    eps = 1
-    fig = MultiRegionFigure()
-    lap = LaplaceMechanism(eps, 1)
-    fig.add_region(region_from_dp_params(eps, 0), "dp no tv")
-    fig.add_region(lap.region_tv(), "tv")
-    fig.add_region(lap.region_exact(), "exact")
-    fig.finish_figure()
-    fig.show_figure()
+        self._window.mainloop()
 
-def gaussianmech():
-    eps = 1.3
-    delta = 0.3
-    fig = MultiRegionFigure()
-    gau = GaussianMechanism(eps, delta, 1)
-    fig.add_region(gau.region_exact(), "exact")
-    fig.add_region(gau.region_tv(), "tv")
-    fig.finish_figure()
-    fig.show_figure()
 
-def randomized_resp():
-    eps = 0.9
-    mech = RandomizedResponse(eps, 5)
-    fig = MultiRegionFigure()
-    fig.add_region(region_from_dp_params(eps, 0), "dp no tv")
-    fig.add_region(mech.region_exact(), "actual")
-    fig.finish_figure()
-    fig.show_figure()
+    def _build_button_privacy(self):
+        def onclick():
+            self._window.destroy()
+            PrivacyWindow()
 
-def composing_dps_intersection_tv(eps = 0.5, delta = 0.1, eta_factor = 0.75):
+        privacy_button = ttk.Button(self._window,
+                                    text="Open privacy window",
+                                    command=lambda: onclick()
+                                    )
+        privacy_button.grid(column=0, row=1)
 
-    eta = eta_factor * (delta + ((np.exp(eps) - 1) * (1 - delta)) / (np.exp(eps) + 1))
+    def _build_combob_utility(self):
+        def onclick(event):
+            curr_val = combob_utilities.get()
+            self._window.destroy()
+            if curr_val == "DP Histogram (ε)":
+                UtilityWindow(DPHistogram, "eps")
+            elif curr_val == "DP Mean (δ)":
+                UtilityWindow(DPMean, "delta")
+            elif curr_val == "DP Mean (ε)":
+                UtilityWindow(DPMean, "delta")
+            else:
+                raise ValueError("Unknown utility")
 
-    intersection = intersect_regions([
-        region_from_dp_composition_exact(eps, delta, 2),  # (eps, delta)-DP x (eps, delta) - DP
-        region_from_dp_params(0, 1 - (1 - eta) * (1 - eta)),  # (0, eta)-DP x (0, eta)-DP
-        region_from_dp_params(eps, 1 - (1-delta) * (1-eta)) # (eps, delta)-DP x (0, eta)-DP
-        ]
-    )
-    factor = 0.2
+        utilities_frame = tk.Frame(self._window, background="white")
 
-    intersection_2 = intersect_regions([
-        region_from_dp_composition_exact(eps, delta, 2),  # (eps, delta)-DP x (eps, delta) - DP
-        region_from_dp_params(0, 1 - (1 - eta) * (1 - eta)),  # (0, eta)-DP x (0, eta)-DP
-        region_from_dp_params(eps, 1 - (1-delta) * (1-eta)), # (eps, delta)-DP x (0, eta)-DP
-        region_from_dp_composition_exact(np.log(((1 - factor * delta)/(1-delta)) * (1 + np.exp(eps)) - 1), delta, 2)
-        ] # instead of 1.0002 * delta : play with another delta between the original delta and eta
-    )
+        combob_utilities = ttk.Combobox(utilities_frame, width=20)
+        combob_utilities['values'] = ("DP Histogram (ε)", "DP Mean (ε)", "DP Mean (δ)")
+        combob_utilities['state'] = 'readonly'
+        combob_utilities.bind('<<ComboboxSelected>>', onclick)
 
-    fig = MultiRegionFigure()
-    #fig.add_region(intersection, "intersection")
-    fig.add_region(intersection_2, "intersection_2")
-    fig.add_region(region_from_dp_composition_exact_total_var(eps, delta, eta, 2), "theorem")
+        utilites_label = tk.Label(utilities_frame, text="Open privacy/utility trade-off window", bg="white")
 
-    fig.finish_figure(f"epsilon={eps}, delta={delta}, eta={eta}")
-    fig.show_figure()
+        utilites_label.pack()
+        combob_utilities.pack()
 
-def composing_dps_intersection_tv_2(eps = 0.9, delta = 0.2, eta = 0.4):
+        utilities_frame.grid(column=0, row=2)
 
-    intersection = intersect_regions([
-        region_from_dp_composition_exact(eps, delta, 2),  # (eps, delta)-DP x (eps, delta) - DP
-        region_from_dp_params(0, 1 - (1 - eta) * (1 - eta)),  # (0, eta)-DP x (0, eta)-DP
-        region_from_dp_composition_simplified([eps, 0], [delta, eta], delta_slack=0.001)  # (eps, delta)-DP x (0, eta)-DP
-        ]
-    )
-
-    fig = MultiRegionFigure()
-    fig.add_region(intersection, "intersection 2")
-    fig.finish_figure(f"epsilon={eps}, delta={delta}, eta={eta}")
-    fig.show_figure()
-
-def composing_dps_theorem_tv(eps = 0.9, delta = 0.2, eta = 0.4):
-    fig = MultiRegionFigure()
-    fig.add_region(region_from_dp_composition_exact_total_var(eps, delta, eta, 2), "theorem")
-    fig.finish_figure()
-    fig.show_figure()
-
-def composing_dps_comp(eps = 0.9, delta = 0.2, eta = 0.4):
-    fig = MultiRegionFigure()
-    fig.add_region(region_from_dp_params(eps, 1 - (1-delta) * (1-eta)), "dp")
-    fig.add_region(region_from_dp_composition_simplified([eps, 0], [delta, eta], delta_slack=0.001), "slack")
-    fig.finish_figure()
-    fig.show_figure()
-
-def composition_heter():
-    eps_1 = 0.5
-    eps_2 = eps_1
-    delta_1 = 0.2
-    delta_2 = delta_1
-
-    fig_1 = MultiRegionFigure()
-    fig_1.add_region(region_from_dp_params(eps_1, delta_1), "1")
-    fig_1.finish_figure("eps1-delta1")
-    fig_1.show_figure()
-
-    fig_2 = MultiRegionFigure()
-    fig_2.add_region(region_from_dp_params(eps_2, delta_2), "2")
-    fig_2.finish_figure("eps2-delta2")
-    fig_2.show_figure()
-
-    fig = MultiRegionFigure(grid_res=1000)
-    ls = []
-    for delta_s in np.logspace(-5, 0, num=500):
-        ls.append(region_from_dp_composition_simplified([eps_1, eps_2], [delta_1, delta_2], delta_s))
-
-    fig.add_region(intersect_regions(ls), "intersection thm 3.5")
-    fig.finish_figure()
-    fig.show_figure()
-
-    fig_prime = MultiRegionFigure()
-    fig_prime.add_region(region_from_dp_composition_exact(eps_1, delta_1, 2), "exact")
-    fig_prime.finish_figure()
-    fig_prime.show_figure()
-
-def superpose():
-    fig = MultiRegionFigure()
-    fig.add_region(region_from_dp_params(0.8, 0), "(0.3, 0)")
-    fig.add_region(region_from_dp_params(0.1, 0.2), "0.1, 0.2)")
-    fig.draw_figure()
-    fig.show_figure()
-
-gdp()
+if __name__ == "__main__":
+    main_window = MainWindow()
